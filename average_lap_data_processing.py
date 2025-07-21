478d01eda8b93c1b76a6303d34af6b0f33f32bcb
EdwinJoshy <100435321+EdwinJoshy@users.noreply.github.com>
2025-07-21 23:56:19 +0530

    Add files via upload

---
+++ average_lap_data_processing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Load data
+df_main = pd.read_csv("data/processed/lap_times_processed.csv")
+
+# Group by raceId, driverId, tyre_compound and aggregate both lap count and average lap time
+df_avg_lap = df_main.groupby(['raceId', 'driverId', 'tyre_compound'], as_index=False).agg({
+    'lap': 'count',
+    'lap_time_seconds': 'mean'
+})
+
+# Rename columns for clarity (optional)
+df_avg_lap.rename(columns={'lap': 'lap_count', 'lap_time_seconds': 'avg_lap_time_seconds'}, inplace=True)
+
+# Preview
+print(df_avg_lap.head(10))
+
+# Save to new CSV
+df_avg_lap.to_csv("data/processed/average_laptime_compoundwise.csv",index=False)
